0cf3b7eafb77d33fa5bead981e523c0dd0c08c21
Pavel <79047358+BONK777@users.noreply.github.com>
2023-12-25 20:43:02 +0300

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -7,7 +7,7 @@ export default [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:1337', 'http://example2']
+      origin: '*'
     }
   },
   'strapi::logger',
